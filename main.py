2beca61ce405d87295b8822d054295de6d24cebc
AVidhanR <itsvidhanreddy@gmail.com>
2024-05-07 14:49:35 +0530

    Created and tested container in streamlit

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import streamlit as st
 
-st.title("AVidhanR")
-st.write("Sample Web App")
-st.button("This is awesome 😎")
+container = st.container()
+container.title("AVidhanR")
+container.write("Sample Web App")
+container.balloons()
+container.button("This is awesome 😎")
