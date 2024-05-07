31f90115c6663c68a74658534c72f1d23007003b
AVidhanR <itsvidhanreddy@gmail.com>
2024-05-07 15:35:44 +0530

    update main.py

--- main.py
+++ main.py
@@ -1,12 +1,32 @@
 import streamlit as st
 
 container = st.container()
-container.title("AVidhanR")
-container.write("A Simple Web App")
-# method 1
-btn = container.button("Contact me")
+container.header("About me")
+
+# * Interesting [streamlit]
+# container.markdown(
+#         """
+#         <style>
+#             /* Define custom CSS styles */
+#             .custom-text {
+#                 font-size: 1.5rem; /* Adjust font size as needed */
+#                 font-weight: bold; /* Make text bold */
+#             }
+#         </style>
+#         <p class = "custom-text">Bio: </p>
+#         """,
+#         unsafe_allow_html = True
+#     )
+container.text(
+    "Hello! I'm Vidhan, A Compute Science undergraduate. \nI love to learn new Stuff and upgrade myself"
+)
+container.markdown(
+    """
+    <a href="https://github.com/AVidhanR" style="font-size: 1rem;">Vist my github</a>
+    """,
+    unsafe_allow_html = True
+)
+btn = container.button("I'm Feeling lucky!")
 if btn:
-    container.balloons()
-# method 2
-# if container.button("Press me"):
-#    container.balloons()
+    st.balloons()
+
